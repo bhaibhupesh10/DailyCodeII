452f496b48485aab019e0e23c380245637243ea8
Bhupesh <bhaibhupesh10@gmail.com>
2022-12-03 22:19:30 +0530

    adding new file from repository

---
+++ FRONTEND/index.js
@@ -0,0 +1,12 @@
+alert("this is javascript program you know na")
+document.write("java script program will be written here with if else statements bole to conditional statement: \n ")
+
+console.log("javascript programming such statements that we can consider: like if, if else, if else if, \n");
+
+a= 20;
+if(a>=18 && a<=21) {
+    document.write("a can do anything: " );
+}
+else{
+    document.write("a can't do anything: ");
+}
