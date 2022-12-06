4106b15dd834f6417c53f55192a95a5527d58c20
Bhupesh <bhaibhupesh10@gmail.com>
2022-12-06 23:28:59 +0530

    updated code

---
+++ FRONTEND/button, if-else ladder/index.js
@@ -0,0 +1,93 @@
+document.write("this is javascript program <br/> you know na ");
+
+document.write("ha na be<BR/> bhai mere hisab se too tu pura hi master karle javascript ko hana ");
+
+document.write("<br/>this is bhupesh choudhary");
+
+
+document.write('<br/> thik hai na be yrr bro');
+document.write("<br/> ANKITA PATEL");
+document.write("<br/>this is program for <br/> ANKITA PATEL<BR/>");
+
+
+var a =2 ;
+
+ if( a>=18 && a<=65){
+document.write("you can vote");
+}
+
+else if(a<18 && a>16){
+document.write("sarpanch chunav ke liye bas kar skta hai ");
+}
+
+else if(a<16 && a>=4)
+{
+document.write("baccha hai be tu to abhi ja dudh pee");
+}
+else if(a>65){
+document.write(" are you still alive ");
+}
+else if(a>=1 && a<=4){
+document.write("infant hai abhi");
+}
+
+else{
+document.write("toddler hai be tu too abhi <br/>");
+}
+
+
+
+
+
+
+
+
+
+
+
+var number1 = 34;
+var number2 = 56;
+sum = number1+number2;
+
+document.write("<br/>" + sum);
+
+
+document.write("<br/>this is functions");
+
+
+
+function avg(a, b) {
+return (a+b)/2;
+}
+c = avg(10, 12);
+
+document.write("<br/>" + c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
