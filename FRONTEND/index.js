bba2804477c02c2ff2f3e807660c0f4ede779fd9
Bhupesh <bhaibhupesh10@gmail.com>
2022-12-09 18:13:45 +0530

    updated code

--- FRONTEND/index.js
+++ FRONTEND/index.js
@@ -27,23 +27,65 @@
 // alert("this is java script program you know better write.....");
 // console.log("this is javascript");
 // document.write("ye webpage me print karne ke liye use hota hai []");
-let age = 12;
-if(age>32)
-{
-    document.write("you are not a kid");
-}
-else if(age>26){
-    console.log("bachhe nahi rhe ");
-}
-else if(age>22)
-{
-    console.log("Yes Bacche nahi rahe");
+// let age = 12;
+// if(age>32)
+// {
+//     document.write("you are not a kid");
+// }
+// else if(age>26){
+//     console.log("bachhe nahi rhe ");
+// }
+// else if(age>22)
+// {
+//     console.log("Yes Bacche nahi rahe");
+//   }
+//     else if(age>18)
+// {
+//     console.log("18 Bacche nahi rahe");
+//     }
+//     else{
+//         document.write("bacche rahe")
+//     }
+// console.log("end of ladder");
+
+// var day = 2;
+
+// switch (day) {
+//     case 0:
+//         document.write("Today is Monday");
+//         break;
+
+//     case 1:
+//         document.write("Today is Tuesday");
+//          break;
+//     case 2:
+//         document.write("Today is Wednesday");
+//         break;
+
+//     case 3:
+//         document.write("Today is Thursday");
+//         break;
+
+//     case 4:
+//         document.write("Today is Friday");
+//         break;
+
+//     case 5:
+//         document.write("Today is Saturday");
+//         break;
+
+//     case 6:
+//         document.write("Today is Sunday");
+//         break;
+        
+//         default:
+//             document.write("Enter the valid Week day");         
+// }
+
+const body = document.querySelector('body');
+  const toggle = document.getElementById('toggle');
+  toggle.onlick = function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
   }
-    else if(age>18)
-{
-    console.log("18 Bacche nahi rahe");
-    }
-    else{
-        document.write("bacche rahe")
-    }
-console.log("end of ladder");
+
