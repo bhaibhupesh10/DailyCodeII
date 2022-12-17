2b45a9a0a7fc4552893fef71c1a4bd80e88d8560
Bhupesh <bhaibhupesh10@gmail.com>
2022-12-17 22:51:28 +0530

    updated code

---
+++ FRONTEND/JS/script.js
@@ -0,0 +1,129 @@
+// let arr = [1,2,3,4,5,6,7];
+// for(var i=0;i<arr.length;i++){
+//     if(i==2){
+//         break;
+//     }
+//     console.log(arr[i])
+// }
+
+// let myArr= ["Fan", "Camera", 34 , Null, true];
+// console.log(myArr.length);
+// myArr.pop();
+// console.log(myArr.length);
+
+
+// var arr2 = [1 ,2,3,4,5,6,7];
+// for(i=0;i<arr.length;i++)
+// {
+//     if(i==2){
+//         continue;
+//     }
+//     console.log(arr[i]);
+// }
+
+
+
+// let j= 0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+
+// let j=10;
+// do{
+//     console.log(arr[j]);
+// }
+
+// while(j<arr.length);
+
+// let myArr = ["Fan", "Camera", 34,null, true];
+// console.log(myArr.length);
+// myArr.pop();    
+// console.log(myArr);
+//  myArr.push("Bhupesh");
+//  console.log(myArr); 
+// myArr.push("harry");
+
+// myArr.shift();
+// console.log(myArr.shift())
+
+// myArr.unshift();
+// console.log(myArr);
+
+// const newLen = myArr.unshift("Harry");
+// console.log(newLen);
+// console.log(myArr);
+
+// let myLovelyString = "Bhupesh is a good boy";
+// console.log(myLovelyString);
+// console.log(myLovelyString.length)
+
+// console.log(myLovelyString.indexOf("Bhupesh"));
+// console.log(myLovelyString.indexOf("good"));
+// console.log(myLovelyString.indexOf("good"));
+// console.log(myLovelyString.slice(0,3));
+
+//replacing the string 
+
+// d= myLovelyString.replace("Bhupesh", "Bhoopesh")
+// console.log(d);
+// console.log(d, myLovelyString);
+
+//we can also replace in (d) the new string 
+// d = d.replace("good", "bad");
+// console.log(d, myLovelyString);
+
+//To print the Date....
+
+// let myDate = new Date();
+// console.log(myDate);
+
+// let myYear = new Year();
+// console.log(myYear);
+
+
+// console.log(myDate.getTime());
+// console.log(myDate.getFullYear());    
+// console.log(myDate.getDate());
+// document.write(myDate.getDate());
+// // document.write(<br/>);
+// document.write(myDate.getMinutes());
+
+// console.log(myDate.getHours());
+// console.log(myDate.getHours());
+
+// console.log(myDate.getHours());
+
+// console.log(myDate.getHours());
+
+// console.log(myDate.getHours())
+
+// console.log(myDate.getHours());
+
+
+// documents.location();
+
+// let elem1 = document.getElementById('click').click();
+// console.log(elem1);
+
+// object.onclick = function(){document.getElementById('container')};
+
+
+// object.addEventListener("click", myScript);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
