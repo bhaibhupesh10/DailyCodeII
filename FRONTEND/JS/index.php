f965a2d94a7b92a97f99ace1ac7196f710f88e06
Bhupesh <bhaibhupesh10@gmail.com>
2022-12-19 01:08:50 +0530

    updated code

---
+++ FRONTEND/JS/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div class="class">ye part html ka hai let se kya hota hai dekhte hai
+    <?php
+    echo "this is first php page ha <br>";
+    echo "<hr>";
+    echo "ha ye kam to kar hi rha hai na be bro";
+?>
+</div>
+
+
+
+</body>
+</html>
